c4399dae809a5bd880ddef27908ca6e7b4366435
TysonG603 <tasibo1324@gmail.com>
2025-11-14 22:19:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,21 @@ document.getElementById('kontrolButonu').addEventListener('click', function() {
 
         document.getElementById('fotografAlbumu').style.display = 'block';
     }
+
 })
+
+function kalpEkle(x, y) {
+    const kalp = document.createElement('span');
+    kalp.classList.add('imlec-kalbi');
+    
+    // Konumu ayarla
+    kalp.style.left = x + 'px';
+    kalp.style.top = y + 'px';
+    
+    document.body.appendChild(kalp);
+    
+    // 1 saniye sonra kalbi kaldır
+    setTimeout(() => {
+        kalp.remove();
+    }, 1000);
+}
